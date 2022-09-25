5377128c81046cca5c2135158ab193d0fc1163f8
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-25 16:57:37 -0300

    creates user register service

--- src/main/java/br/com/financeiro/service/user/RegisterUserService.java
+++ src/main/java/br/com/financeiro/service/user/RegisterUserService.java
@@ -1,2 +1,36 @@
-package br.com.financeiro.service.user;public class RegisterUserService {
+package br.com.financeiro.service.user;
+
+import br.com.financeiro.controller.request.user.RegisterUserRequest;
+import br.com.financeiro.controller.response.UserResponse;
+import br.com.financeiro.mapper.user.RegisterUserMapper;
+import br.com.financeiro.model.User;
+import br.com.financeiro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import static br.com.financeiro.mapper.user.RegisterUserMapper.toEntity;
+
+@Service
+public class RegisterUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserResponse register(RegisterUserRequest request) {
+
+        User user = toEntity(request);
+        user.setPassword(getEncryptedPassword(request.getPassword()));
+
+        userRepository.save(user);
+
+        return RegisterUserMapper.toResponse(user);
+    }
+
+    public String getEncryptedPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
 }
