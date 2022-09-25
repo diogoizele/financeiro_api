85935c5a423c7b70c5f377c3fc95aa2c4a4bfc07
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-25 16:29:15 -0300

    adds activity repository to connect api with db

---
+++ src/main/java/br/com/financeiro/repository/ActivityRepository.java
@@ -0,0 +1,13 @@
+package br.com.financeiro.repository;
+
+import br.com.financeiro.controller.response.activity.GetActivityResponse;
+import br.com.financeiro.model.Activity;
+import br.com.financeiro.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    Page<Activity> findAllByUserIdAndCategory(Long userId, Category category, Pageable pageable);
+}
