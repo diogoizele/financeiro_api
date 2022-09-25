0bd12b8fe1d6bfd41d991dae73d0231d7ecaeb08
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-25 16:56:39 -0300

    adds purchase model

--- src/main/java/br/com/financeiro/model/Purchase.java
+++ src/main/java/br/com/financeiro/model/Purchase.java
@@ -1,2 +1,33 @@
-package br.com.financeiro.model;public class Purchase {
+package br.com.financeiro.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Builder @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id") @ToString(of = "id")
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String item;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String link;
+    
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private User user;
 }
