1f703487bee3e9ecaeafd16389a1e71a2824cd2c
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-25 16:57:02 -0300

    adds register endpoint to make user register

--- src/main/java/br/com/financeiro/controller/AuthController.java
+++ src/main/java/br/com/financeiro/controller/AuthController.java
@@ -1,11 +1,16 @@
 package br.com.financeiro.controller;
 
+import br.com.financeiro.controller.request.user.RegisterUserRequest;
 import br.com.financeiro.controller.response.UserResponse;
+import br.com.financeiro.service.user.RegisterUserService;
 import br.com.financeiro.service.user.UserAuthenticatedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
@@ -14,8 +19,16 @@ public class AuthController {
     @Autowired
     private UserAuthenticatedService userAuthenticatedService;
 
+    @Autowired
+    private RegisterUserService registerUserService;
+
     @PostMapping("/login")
     public UserResponse auth() {
         return userAuthenticatedService.getReponse();
+    }
+
+    @PostMapping("/register")
+    public UserResponse register(@Valid  @RequestBody RegisterUserRequest request) {
+        return registerUserService.register(request);
     }
 }
