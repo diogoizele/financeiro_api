534942e7b7892df0dfd2cb735865ce5374bd5ad0
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-25 16:57:23 -0300

    creates user register mapper and request

--- src/main/java/br/com/financeiro/controller/request/user/RegisterUserRequest.java
+++ src/main/java/br/com/financeiro/controller/request/user/RegisterUserRequest.java
@@ -1,2 +1,15 @@
-package br.com.financeiro.controller.request.user;public class RegisterUserRequest {
+package br.com.financeiro.controller.request.user;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Builder @NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class RegisterUserRequest {
+
+    private String name;
+    private String password;
+    private BigDecimal wage;
 }
--- src/main/java/br/com/financeiro/mapper/user/RegisterUserMapper.java
+++ src/main/java/br/com/financeiro/mapper/user/RegisterUserMapper.java
@@ -1,2 +1,23 @@
-package br.com.financeiro.mapper.user;public class RegisterUserMapper {
+package br.com.financeiro.mapper.user;
+
+import br.com.financeiro.controller.request.user.RegisterUserRequest;
+import br.com.financeiro.controller.response.UserResponse;
+import br.com.financeiro.model.User;
+
+public class RegisterUserMapper {
+
+    public static User toEntity(RegisterUserRequest request) {
+        return User.builder()
+                .name(request.getName())
+                .wage(request.getWage())
+                .build();
+    }
+
+    public static UserResponse toResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .wage(user.getWage())
+                .build();
+    }
 }
