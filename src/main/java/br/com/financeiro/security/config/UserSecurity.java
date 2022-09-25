6bd437acc426ee0f7e0ee0a7d7bfbef393b4cca4
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-25 16:29:53 -0300

    adds security config classes

---
+++ src/main/java/br/com/financeiro/security/config/UserSecurity.java
@@ -0,0 +1,37 @@
+package br.com.financeiro.security.config;
+
+import br.com.financeiro.model.User;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class UserSecurity implements UserDetails {
+
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final List<SimpleGrantedAuthority> authorities;
+
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+
+    public UserSecurity(User user) {
+
+        this.id = user.getId();
+        this.username = user.getName();
+        this.password = user.getPassword();
+
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+
+        this.authorities = new ArrayList<>();
+    }
+}
